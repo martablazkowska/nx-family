dbca29b1aa07733bc0dee0e040798004c4a75572
Marta Błażkowska <marta@christchurchcasino.com>
2025-03-06 22:38:42 +0100

    pwa

--- apps/family/next.config.js
+++ apps/family/next.config.js
@@ -1,22 +1,12 @@
-//@ts-check
+/** @type {import('next').NextConfig} */
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
-/**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
- **/
-const nextConfig = {
-  nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
-};
-
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = withPWA({
+  reactStrictMode: true,
+});
