e58c403d81c0d6fae4ef5bbf8d54defb1aad4092
Marta Błażkowska <marta@christchurchcasino.com>
2025-03-06 22:44:00 +0100

    pwa

--- apps/family/app/layout.tsx
+++ apps/family/app/layout.tsx
@@ -20,6 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+          <link
+              rel="manifest"
+              href="/manifest.json"
+          />
+      </head>
       <body>
         <ThemeProvider>
           <div className={styles.layout}>
